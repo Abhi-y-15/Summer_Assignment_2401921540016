69bc730ce66c7dc339ddf9f2da910a7db4f1aeef
Abhi-y-15 <ay1520050000@gmail.com>
2026-06-02 15:16:04 +0530

    all done day 2

--- week_1/Day_2/containsDuplicate.java
+++ week_1/Day_2/containsDuplicate.java
@@ -1,3 +1,20 @@
-public class containsDuplicate {
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i = 1 ;i<nums.length-1 ;i++){
+           int j = i+1;
+           while(j>0 && nums[j] < nums[j - 1]){
+                int temp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1]= temp;
+                j--;
+            }
+        }
+       for(int i = 1 ;i<nums.length ; i++){
+          if(nums[i] == nums[i-1]){
+            return true;
+          }
+        }
+       return false ;
+    }
     
 }
--- week_1/Day_2/maxAvgSubarray.java
+++ week_1/Day_2/maxAvgSubarray.java
@@ -1,5 +1,17 @@
-package week_1.Day_2;
-
-public class maxAvgSubarray {
-    
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int j =0;
+        int cSum = 0;
+        while(j<k){
+            cSum = cSum+nums[j++];   
+        }
+        int maxSum = cSum;
+        for(int i = 1;i<=nums.length-k ;i++){
+            cSum = cSum - nums[i-1];
+            cSum = cSum+nums[i+k-1] ;
+            maxSum = Math.max(cSum , maxSum);
+        }
+        double avg = (double)maxSum/k;
+        return avg ;
+    }
 }
