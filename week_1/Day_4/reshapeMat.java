aa4ca432493aefc971b882fdd9bf2b004986261f
Abhi-y-15 <ay1520050000@gmail.com>
2026-06-04 16:46:58 +0530

    q2 day 4

--- week_1/Day_4/reshapeMat.java
+++ week_1/Day_4/reshapeMat.java
@@ -1,3 +1,22 @@
-public class reshapeMat {
-    
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int x = mat.length;
+        int y = mat[0].length;
+        if((x*y) != (c*r)) return mat; 
+        int[][]ans = new int[r][c];
+        int[]temp = new int[x*y];
+        int k = 0;
+        for(int i = 0;i<x;i++){
+            for(int j=0;j<y;j++){
+                temp[k++] = mat[i][j];
+            }
+        }
+        k = 0;
+        for(int i = 0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ans[i][j] = temp[k++];
+            }
+        }
+        return ans ;
+    }
 }
